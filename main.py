c97537da0f6618d577c9edfe0c75d43e15a6b7d6
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2024-06-19 16:54:48 -0700

    write basic argparse logic

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
 import shazamio
+
+## Parse Args
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="path to the .mp4 or .mp3 file")
+args = parser.parse_args()
+
+## Diagnostics
+assert os.path.exists(args.path), "File does not exist!"
+filetype = args.path.split('.')[-1]
+print(f"Working with file: {args.path}\n"
+      f"File type: .{filetype}\n")
+
+
 
 print("I do nothing yet...")
